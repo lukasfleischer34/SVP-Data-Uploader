9ba38716d64c9e9196ed9f910e71e1740675ff92
lukasfleischer34 <lukasjrfleischer@gmail.com>
2026-07-07 18:15:56 -0600

    change from files to google sheets
    Added function to load data from Google Sheets into variables.

--- ExportDataGoogle.py
+++ ExportDataGoogle.py
@@ -15,6 +15,53 @@ client = gspread.authorize(creds)
 spreadsheet_id = "1Ln8fVHSCOueiw64-6IrFMGokYRHoewe-EiVObFDfoEg"
 sheet = client.open_by_key(spreadsheet_id).sheet1
 
+def load_from_google():
+    data = sheet.get_all_values()
+
+    players = []
+    heartsgp = []
+    heartsw = []
+    heartsl = []
+    crownsgp = []
+    crownsw = []
+    crownsl = []
+    dicegp = []
+    dicew = []
+    dicel = []
+    dicenp = []
+    diceiw = []
+    olymgp = []
+    olymw = []
+    olymg = []
+    olyms = []
+    olymb = []
+
+    for row in data[1:]:  
+        players.append(row[0])
+        heartsgp.append(int(row[1]))
+        heartsw.append(int(row[2]))
+        heartsl.append(int(row[3]))
+        crownsgp.append(int(row[4]))
+        crownsw.append(int(row[5]))
+        crownsl.append(int(row[6]))
+        dicegp.append(int(row[7]))
+        dicew.append(int(row[8]))
+        dicel.append(int(row[9]))
+        dicenp.append(int(row[10]))
+        diceiw.append(int(row[11]))
+        olymgp.append(int(row[12]))
+        olymw.append(int(row[13]))
+        olymg.append(int(row[14]))
+        olyms.append(int(row[15]))
+        olymb.append(int(row[16]))
+
+    return (
+        players, heartsgp, heartsw, heartsl,
+        crownsgp, crownsw, crownsl,
+        dicegp, dicew, dicel, dicenp, diceiw,
+        olymgp, olymw, olymg, olyms, olymb
+    )
+
 
 def export_to_google(players, heartsgp, heartsw, heartsl,
                     crownsgp, crownsw, crownsl,
